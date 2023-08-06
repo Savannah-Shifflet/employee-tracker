48764e6d16a704088d3521ad676802d4ad5d87ab
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-05 22:22:55 -0500

    create add department and add role functions

--- db/index.js
+++ db/index.js
@@ -29,7 +29,7 @@ viewAllRoles();
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 // WHEN I choose to view all employees
 const viewAllEmployees = () => {
-    db.query('SELECT employee.id AS Employee_id, employee.first_name AS First_Name, FROM employee JOIN role ON role.id = employee.role_id JOIN department ON department.id = role.department_id', (err, results) => { 
+    db.query('SELECT employee_individual.id AS Employee_id, employee_individual.first_name AS First_Name, employee_individual.last_name AS Last_Name, role.title AS Job_Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, \' \', manager.last_name) AS Manager_Name FROM employee employee_individual JOIN role ON role.id = employee_individual.role_id JOIN department ON department.id = role.department_id JOIN employee manager ON manager.manager_id = employee_individual.id', (err, results) => { 
         if(err) {
             console.log(err);
         }
@@ -39,12 +39,47 @@ const viewAllEmployees = () => {
 viewAllEmployees();
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 // WHEN I choose to add a department
+const addDepartment = (name) => {
+    db.query('INSERT INTO department (name) VALUES (?)', name, (err, results) => { 
+        if(err) {
+            console.log(err);
+        }
+        console.log(`${name} was successfully added to departments!`);
+    });
+}
+
+// addDepartment('Service');
 // THEN I am prompted to enter the name of the department and that department is added to the database
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+const addRole = async (title, salary, department) => {
+    const departmentID = '';
+    db.promise().query({sql: 'SELECT id FROM department WHERE name = ?', rowsAsArray: true}, [department])
+    
+    // (err, results) => { 
+    //     if(err) {
+    //     console.log(err);
+    //     }
+    //     departmentID = results;
+    //     console.log(departmentID);
+    // })
+    .then(([rows, field]) => {
+        db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, rows], (err, results) => { 
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(`${title} was successfully added to roles!`);
+                }
+            });
+        }
+    );
+};
+
+// addRole('Manager', '160000', 'Sales');
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
-module.exports = { viewAllDepartments };
+module.exports = { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole};

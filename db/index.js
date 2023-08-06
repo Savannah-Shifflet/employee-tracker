b133ec34e8fa26fccf9178b7946287468633edf9
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-05 22:51:46 -0500

    created add Employee function

--- db/index.js
+++ db/index.js
@@ -29,7 +29,7 @@ viewAllRoles();
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 // WHEN I choose to view all employees
 const viewAllEmployees = () => {
-    db.query('SELECT employee_individual.id AS Employee_id, employee_individual.first_name AS First_Name, employee_individual.last_name AS Last_Name, role.title AS Job_Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, \' \', manager.last_name) AS Manager_Name FROM employee employee_individual JOIN role ON role.id = employee_individual.role_id JOIN department ON department.id = role.department_id JOIN employee manager ON manager.manager_id = employee_individual.id', (err, results) => { 
+    db.query('SELECT employee_individual.id AS Employee_id, employee_individual.first_name AS First_Name, employee_individual.last_name AS Last_Name, role.title AS Job_Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, \' \', manager.last_name) AS Manager_Name FROM employee employee_individual JOIN role ON role.id = employee_individual.role_id JOIN department ON department.id = role.department_id LEFT JOIN employee manager ON manager.manager_id = employee_individual.id', (err, results) => { 
         if(err) {
             console.log(err);
         }
@@ -53,16 +53,7 @@ const addDepartment = (name) => {
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 const addRole = async (title, salary, department) => {
-    const departmentID = '';
-    db.promise().query({sql: 'SELECT id FROM department WHERE name = ?', rowsAsArray: true}, [department])
-    
-    // (err, results) => { 
-    //     if(err) {
-    //     console.log(err);
-    //     }
-    //     departmentID = results;
-    //     console.log(departmentID);
-    // })
+    db.promise().query({sql: 'SELECT id FROM department WHERE name = ?', rowsAsArray: true}, department)
     .then(([rows, field]) => {
         db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, rows], (err, results) => { 
                 if(err) {
@@ -78,8 +69,30 @@ const addRole = async (title, salary, department) => {
 // addRole('Manager', '160000', 'Sales');
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
+const addEmployee = async (first_name, last_name, role, manager) => {
+    let roleId = 0;
+    let managerId = 0;
+    db.promise().query({sql: 'SELECT id FROM role WHERE title = ?', rowsAsArray: true}, role)
+    .then(([rows, field]) => {
+        roleId = rows;
+        const managerName = manager.split(' '); 
+        console.log(managerName);
+        db.promise().query({sql: 'SELECT id FROM employee WHERE first_name = ? AND last_name = ?', rowsAsArray: true}, [managerName[0], managerName[1]])
+        .then(([rows, field]) => {
+            managerId = rows; 
+        })
+        .then(() => {
+            db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, roleId, managerId], (err, results) => { 
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(`${first_name} ${last_name} was successfully added to roles!`);
+                }
+            });
+        });
+    });
+}; 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
-module.exports = { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole};
+module.exports = { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee };
